91dc25fa526064e4570221b41a9a072f26402351
bk137 <641664923@qq.com>
2020-06-17 14:43:19 +0800

    2020-6-17

---
+++ src/views/common/home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="mod-home">
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        srcIMG: 'https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg'
+      }
+    }
+  }
+</script>
+
+<style>
+  .mod-home {
+    line-height: 1.5;
+    background-image: url('/static/img/sanji.jpg');
+    width:1920px;
+    height: 1080px;
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+</style>
+
